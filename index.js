46dcb2d5a31c272040c5350c7f5a91fe7f67e1a3
Janakmatov <jan.akmatov98@gmail.com>
2023-05-22 19:46:01 -0500

    added

---
+++ index.js
@@ -0,0 +1,6 @@
+let childrenNumber = 2;
+let parentName = "leya";
+let country = "Chicaho";
+let jobTitle = "developer";
+let fortuneTeller = "You will be " + jobTitle + " in " + country + " and married to " + parentName + " with " + childrenNumber + " kids.";
+console.log(fortuneTeller);
